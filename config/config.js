055211045561c6e0f8520f7a2649636e5e5321b2
BabyGrootTheEpic <62159575+BabyGrootTheEpic@users.noreply.github.com>
2022-02-20 18:46:46 -0600

    Teambuilder shows CAP as illegal

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (9c3604db)";
+Config.version = "0.11.2 (8473fbe1)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
